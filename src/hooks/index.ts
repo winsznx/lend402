f551feed004ff74bfa880ddf91a556d3a6ef2568
winsznx <timjosh507@gmail.com>
2026-04-15 10:24:00 +0100

    chore(hooks): add barrel export for all hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,7 @@
+export { useDebounce } from "./useDebounce";
+export { useMediaQuery } from "./useMediaQuery";
+export { useClickOutside } from "./useClickOutside";
+export { useLocalStorage } from "./useLocalStorage";
+export { useScrollLock } from "./useScrollLock";
+export { useCopyToClipboard } from "./useCopyToClipboard";
+export { useIsMounted } from "./useIsMounted";
