8a712ebfa60e0ee5e253b827026bacf3abd3524b
winsznx <timjosh507@gmail.com>
2026-04-15 13:20:48 +0100

    chore(types): add barrel export for all shared types

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+export type { VaultRecord, CallRecord } from "./vault";
+export type {
+  Caip2NetworkId,
+  PaymentScheme,
+  PaymentOption,
+  PaymentRequiredBody,
+  XPaymentHeader,
+  XPaymentResponse,
+  SettlementRequest,
+  SettlementReceipt,
+} from "./x402";
+export type {
+  Database,
+  VaultRow,
+  CallRow,
+  DashboardCallRow,
+  VaultWithRecentCalls,
+} from "./database";
