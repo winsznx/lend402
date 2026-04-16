e88b1da6407e0eec0a6cf31ff4636aa718ccf3ff
winsznx <timjosh507@gmail.com>
2026-04-16 20:39:10 +0100

    chore(lib): update barrel export to include new utilities

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 export { cn } from "./cn";
 export { HTTP_STATUS, type HttpStatus } from "./http-status";
+export { HTTP_METHODS, type HttpMethod, isSafeMethod } from "./http-method";
 export { jsonError } from "./api-error";
 export {
   truncateMiddle,
@@ -22,3 +23,25 @@ export {
   buildExplorerContractUrl,
   buildVaultProxyUrl,
 } from "./url";
+export { buildQueryString, parseQueryString } from "./query-string";
+export { fetchJson } from "./fetch-json";
+export { combineAbortSignals, timeoutSignal } from "./abort";
+export { safeJsonParse, tryJsonParse } from "./json";
+export { escapeHtml, stripHtml } from "./html";
+export { chunk, range, zip, partition, last, compact } from "./array";
+export { pick, omit, mapValues, isEmptyObject } from "./object";
+export { deepEqual } from "./deep-equal";
+export { deepClone } from "./deep-clone";
+export { parseCsv, parseCsvLine, toCsv } from "./csv";
+export { clamp, roundTo, lerp, inverseLerp, sum, average } from "./numeric";
+export { capitalize, toTitleCase, toCamelCase, toKebabCase, toSnakeCase, reverseString, countOccurrences } from "./string";
+export { logger } from "./logger";
+export { getOrGenerateRequestId, REQUEST_ID_HEADER } from "./request-id";
+export { serializeCookie, parseCookie } from "./cookies";
+export { formatUsd, formatCompact, formatPercent } from "./intl";
+export { secondsAgo, toIsoString, toUnix, addSeconds, isExpired } from "./date";
+export { generateRandomId, generateShortId } from "./random-id";
+export { getErrorMessage, HttpError, ValidationError } from "./errors";
+export { buildCorsHeaders } from "./cors";
+export { CONTENT_TYPES, isJsonContentType, isFormContentType } from "./content-type";
+export { parseBearerToken, timingSafeStringCompare } from "./bearer";
