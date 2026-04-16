e6e6335be33003c2acf60cb39d63fe2ebf4d45b5
winsznx <timjosh507@gmail.com>
2026-04-16 20:39:10 +0100

    feat(lib): add x402 version assertion helpers

---
+++ src/lib/x402-version.ts
@@ -0,0 +1,11 @@
+import { X402_VERSION } from "./protocol";
+
+export function assertX402Version(version: unknown): asserts version is 2 {
+  if (version !== X402_VERSION) {
+    throw new Error("Unsupported x402 version: " + String(version));
+  }
+}
+
+export function isValidX402Version(version: unknown): version is 2 {
+  return version === X402_VERSION;
+}
