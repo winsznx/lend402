80a7c569c75fe2de7e01ad4afa73ee89df402c11
winsznx <timjosh507@gmail.com>
2026-04-26 03:14:22 +0100

    refactor(style): enforce absolute paths and strict typing in index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,33 +1,33 @@
-export { useDebounce } from "./useDebounce";
-export { useMediaQuery } from "./useMediaQuery";
-export { useClickOutside } from "./useClickOutside";
-export { useLocalStorage } from "./useLocalStorage";
-export { useScrollLock } from "./useScrollLock";
-export { useCopyToClipboard } from "./useCopyToClipboard";
-export { useIsMounted } from "./useIsMounted";
+export { useDebounce } from "@/hooks/useDebounce";
+export { useMediaQuery } from "@/hooks/useMediaQuery";
+export { useClickOutside } from "@/hooks/useClickOutside";
+export { useLocalStorage } from "@/hooks/useLocalStorage";
+export { useScrollLock } from "@/hooks/useScrollLock";
+export { useCopyToClipboard } from "@/hooks/useCopyToClipboard";
+export { useIsMounted } from "@/hooks/useIsMounted";
 
-export { useInterval } from "./useInterval";
+export { useInterval } from "@/hooks/useInterval";
 
-export { usePrevious } from "./usePrevious";
+export { usePrevious } from "@/hooks/usePrevious";
 
-export { useToggle } from "./useToggle";
+export { useToggle } from "@/hooks/useToggle";
 
-export { useWindowSize } from "./useWindowSize";
+export { useWindowSize } from "@/hooks/useWindowSize";
 
-export { useOnlineStatus } from "./useOnlineStatus";
+export { useOnlineStatus } from "@/hooks/useOnlineStatus";
 
-export { useAsync } from "./useAsync";
-export { useCountdown } from "./useCountdown";
-export { useTimeout } from "./useTimeout";
-export { useEventListener } from "./useEventListener";
-export { useKeyPress } from "./useKeyPress";
-export { useHover } from "./useHover";
-export { useMount } from "./useMount";
-export { useUnmount } from "./useUnmount";
-export { useDocumentTitle } from "./useDocumentTitle";
-export { useUpdate } from "./useUpdate";
-export { useSessionStorage } from "./useSessionStorage";
-export { useFocus } from "./useFocus";
-export { useIntersectionObserver } from "./useIntersectionObserver";
-export { useDebouncedCallback } from "./useDebouncedCallback";
-export { useForm } from "./useForm";
+export { useAsync } from "@/hooks/useAsync";
+export { useCountdown } from "@/hooks/useCountdown";
+export { useTimeout } from "@/hooks/useTimeout";
+export { useEventListener } from "@/hooks/useEventListener";
+export { useKeyPress } from "@/hooks/useKeyPress";
+export { useHover } from "@/hooks/useHover";
+export { useMount } from "@/hooks/useMount";
+export { useUnmount } from "@/hooks/useUnmount";
+export { useDocumentTitle } from "@/hooks/useDocumentTitle";
+export { useUpdate } from "@/hooks/useUpdate";
+export { useSessionStorage } from "@/hooks/useSessionStorage";
+export { useFocus } from "@/hooks/useFocus";
+export { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
+export { useDebouncedCallback } from "@/hooks/useDebouncedCallback";
+export { useForm } from "@/hooks/useForm";
